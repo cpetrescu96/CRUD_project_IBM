4b478972ad8db6549066ce74502eea7f0147bea1
cpetrescu96 <cristi.vl96@gmail.com>
2019-03-21 12:34:16 +0200

    Files added for IBM project

---
+++ main.js
@@ -0,0 +1,451 @@
+let users = [{
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+            "geo": {
+                "lat": "-43.9509",
+                "lng": "-34.4618"
+            }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+            "name": "Deckow-Crist",
+            "catchPhrase": "Proactive didactic contingency",
+            "bs": "synergize scalable supply-chains"
+        }
+    },
+    {
+        "id": 3,
+        "name": "Clementine Bauch",
+        "username": "Samantha",
+        "email": "Nathan@yesenia.net",
+        "address": {
+            "street": "Douglas Extension",
+            "suite": "Suite 847",
+            "city": "McKenziehaven",
+            "zipcode": "59590-4157",
+            "geo": {
+                "lat": "-68.6102",
+                "lng": "-47.0653"
+            }
+        },
+        "phone": "1-463-123-4447",
+        "website": "ramiro.info",
+        "company": {
+            "name": "Romaguera-Jacobson",
+            "catchPhrase": "Face to face bifurcated interface",
+            "bs": "e-enable strategic applications"
+        }
+    },
+    {
+        "id": 4,
+        "name": "Patricia Lebsack",
+        "username": "Karianne",
+        "email": "Julianne.OConner@kory.org",
+        "address": {
+            "street": "Hoeger Mall",
+            "suite": "Apt. 692",
+            "city": "South Elvis",
+            "zipcode": "53919-4257",
+            "geo": {
+                "lat": "29.4572",
+                "lng": "-164.2990"
+            }
+        },
+        "phone": "493-170-9623 x156",
+        "website": "kale.biz",
+        "company": {
+            "name": "Robel-Corkery",
+            "catchPhrase": "Multi-tiered zero tolerance productivity",
+            "bs": "transition cutting-edge web services"
+        }
+    },
+    {
+        "id": 5,
+        "name": "Chelsey Dietrich",
+        "username": "Kamren",
+        "email": "Lucio_Hettinger@annie.ca",
+        "address": {
+            "street": "Skiles Walks",
+            "suite": "Suite 351",
+            "city": "Roscoeview",
+            "zipcode": "33263",
+            "geo": {
+                "lat": "-31.8129",
+                "lng": "62.5342"
+            }
+        },
+        "phone": "(254)954-1289",
+        "website": "demarco.info",
+        "company": {
+            "name": "Keebler LLC",
+            "catchPhrase": "User-centric fault-tolerant solution",
+            "bs": "revolutionize end-to-end systems"
+        }
+    },
+    {
+        "id": 6,
+        "name": "Mrs. Dennis Schulist",
+        "username": "Leopoldo_Corkery",
+        "email": "Karley_Dach@jasper.info",
+        "address": {
+            "street": "Norberto Crossing",
+            "suite": "Apt. 950",
+            "city": "South Christy",
+            "zipcode": "23505-1337",
+            "geo": {
+                "lat": "-71.4197",
+                "lng": "71.7478"
+            }
+        },
+        "phone": "1-477-935-8478 x6430",
+        "website": "ola.org",
+        "company": {
+            "name": "Considine-Lockman",
+            "catchPhrase": "Synchronised bottom-line interface",
+            "bs": "e-enable innovative applications"
+        }
+    },
+    {
+        "id": 7,
+        "name": "Kurtis Weissnat",
+        "username": "Elwyn.Skiles",
+        "email": "Telly.Hoeger@billy.biz",
+        "address": {
+            "street": "Rex Trail",
+            "suite": "Suite 280",
+            "city": "Howemouth",
+            "zipcode": "58804-1099",
+            "geo": {
+                "lat": "24.8918",
+                "lng": "21.8984"
+            }
+        },
+        "phone": "210.067.6132",
+        "website": "elvis.io",
+        "company": {
+            "name": "Johns Group",
+            "catchPhrase": "Configurable multimedia task-force",
+            "bs": "generate enterprise e-tailers"
+        }
+    },
+    {
+        "id": 8,
+        "name": "Nicholas Runolfsdottir V",
+        "username": "Maxime_Nienow",
+        "email": "Sherwood@rosamond.me",
+        "address": {
+            "street": "Ellsworth Summit",
+            "suite": "Suite 729",
+            "city": "Aliyaview",
+            "zipcode": "45169",
+            "geo": {
+                "lat": "-14.3990",
+                "lng": "-120.7677"
+            }
+        },
+        "phone": "586.493.6943 x140",
+        "website": "jacynthe.com",
+        "company": {
+            "name": "Abernathy Group",
+            "catchPhrase": "Implemented secondary concept",
+            "bs": "e-enable extensible e-tailers"
+        }
+    },
+    {
+        "id": 9,
+        "name": "Glenna Reichert",
+        "username": "Delphine",
+        "email": "Chaim_McDermott@dana.io",
+        "address": {
+            "street": "Dayna Park",
+            "suite": "Suite 449",
+            "city": "Bartholomebury",
+            "zipcode": "76495-3109",
+            "geo": {
+                "lat": "24.6463",
+                "lng": "-168.8889"
+            }
+        },
+        "phone": "(775)976-6794 x41206",
+        "website": "conrad.com",
+        "company": {
+            "name": "Yost and Sons",
+            "catchPhrase": "Switchable contextually-based project",
+            "bs": "aggregate real-time technologies"
+        }
+    },
+    {
+        "id": 10,
+        "name": "Clementina DuBuque",
+        "username": "Moriah.Stanton",
+        "email": "Rey.Padberg@karina.biz",
+        "address": {
+            "street": "Kattie Turnpike",
+            "suite": "Suite 198",
+            "city": "Lebsackbury",
+            "zipcode": "31428-2261",
+            "geo": {
+                "lat": "-38.2386",
+                "lng": "57.2232"
+            }
+        },
+        "phone": "024-648-3804",
+        "website": "ambrose.net",
+        "company": {
+            "name": "Hoeger LLC",
+            "catchPhrase": "Centralized empowering task-force",
+            "bs": "target end-to-end models"
+        }
+    }
+];
+
+
+
+$(document).ready(function () {
+
+    $(document).on("click",initInteractions);
+
+    let container = $("#Users");
+    let containerModals = $(`.updateModals`);
+
+    function loadModals(containerModals){
+        users.forEach(function (user, i) {
+            let html = `<!-- Edit Modal form -->
+            <div class="modal fade" id="editUser${i}" tabindex="-1" role="dialog" aria-labelledby="editUser${i}Title" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">Update User Info</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-sm-8">
+    
+                                    <h4 class="page-header">Preview Detailes</h4>
+                                    <form role="form">
+                                        <div class="form-group float-label-control">
+                                            <label for="">Company</label>
+                                            <input type="email" class="form-control" placeholder="${user.company}">
+                                        </div>
+                                        <div class="form-group float-label-control">
+                                            <label for="">Email</label>
+                                            <input type="email" class="form-control" aria-describedby="emailHelp"
+                                                placeholder="${user.email}">
+                                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with
+                                                anyone else.</small>
+                                        </div>
+                                        <div class="form-group float-label-control">
+                                            <label for="">Phone</label>
+                                            <textarea class="form-control" placeholder="${user.phone}"
+                                                rows="1"></textarea>
+                                        </div>
+                                        <div class="form-group float-label-control">
+                                            <label for="">Website</label>
+                                            <textarea class="form-control" placeholder="${user.email}"
+                                                rows="1"></textarea>
+                                        </div>
+                                    </form>
+    
+                                    <h4 class="page-header">Adress</h4>
+                                    <form role="form">
+                                        <div class="form-group float-label-control label-bottom">
+                                            <label for="">Street</label>
+                                            <input type="email" class="form-control" placeholder="${user.address.street}">
+                                        </div>
+                                        <div class="form-group float-label-control label-bottom">
+                                            <label for="">Suite</label>
+                                            <input type="email" class="form-control" placeholder="${user.address.suite}">
+                                        </div>
+                                        <div class="form-group float-label-control label-bottom">
+                                            <label for="">City</label>
+                                            <input type="email" class="form-control" placeholder="${user.address.city}">
+                                        </div>
+                                        <div class="form-group float-label-control label-bottom">
+                                            <label for="">Zipcode</label>
+                                            <input type="email" class="form-control" placeholder="${user.address.zipcode}">
+                                        </div>
+                                        <div class="form-group float-label-control label-bottom">
+                                            <label for="">Geo</label>
+                                            <input type="email" class="form-control" placeholder="${parseInt(user.geo.lat)}">
+                                            <input type="email" class="form-control" placeholder="${parseInt(user.geo.lng)}">
+                                        </div>
+    
+                                    </form>
+    
+    
+                                    <h4 class="page-header">Company Details</h4>
+                                    <form role="form">
+                                        <div class="form-group float-label-control label-bottom">
+                                            <label for="">Catch Prase</label>
+                                            <input type="email" class="form-control" placeholder="${user.company.catchPhrase}">
+                                        </div>
+                                        <div class="form-group float-label-control label-bottom">
+                                            <label for="">Aria of interest</label>
+                                            <input type="email" class="form-control" placeholder="${user.company.bs}">
+                                        </div>
+    
+                                </div>
+                                <div class="col-sm-4">
+    
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary" id="editUser${i}">Save Changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+
+            containerModals.html(html);
+
+        });
+    }
+
+    function loadUsers(container) {
+        let html = `<div id="ContainerOfCards" class="row">`;
+
+        users.forEach(function (user, i) {
+            html += `<div class="col-md-4 col-sm-12 " id="BusinessCard${i}" data-userToBeDeleted="${i}" uid="${i}">
+                <div class="card spacing">
+            
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item active card-title">
+                            <h4>${user.name}</h4><br>
+                            <button type="button" id="delete${i}" did="${i}" class="btn btn-danger buttons delete${i}">DELETE</button>
+                            <button type="button" id="edit${i}" eid="${i}" class="btn btn-secondary buttons" data-toggle="modal"
+                            data-target="#editUser"> <p class="edit">EDIT</p>
+                            </button>
+                                            
+
+                            
+                            
+                        </li>
+                        <li class="list-group-item">Company: <br>${user.company.name}</li>
+                        <li class="list-group-item">Website: <br>${user.website}</li>
+                        <li class="list-group-item">Username: <br>${user.username}</li>
+                    </ul>
+                    <div class="card-body">
+                        <a class="card-link" id="catch" data-toggle="collapse" href="#profile${i}" role="button"
+                            aria-expanded="false" aria-controls="profile${i}">
+                            See more
+                        </a>
+                        <div class="collapse" id="profile${i}">
+                            <ul class="list-group list-group-flush">
+            
+                                <li class="list-group-item">username: ${user.username}</li>
+                                <li class="list-group-item">Email: ${user.email}</li>
+                                <li class="list-group-item">Phone: ${user.phone}</li>
+                                <li class="list-group-item">Address -
+                                    <a class="card-link" id="catch" data-toggle="collapse" href="#address${i}" role="button"
+                                        aria-expanded="false" aria-controls="address${i}">
+                                        See more
+                                    </a>
+            
+                                    <div class="collapse" id="address${i}">
+                                        <ul class="list-group list-group-flush">
+            
+                                            <li class="list-group-item">Street: ${user.address.street}</li>
+                                            <li class="list-group-item">Suite: ${user.address.suite}</li>
+                                            <li class="list-group-item">City: ${user.address.city}</li>
+                                            <li class="list-group-item">Zipcode: ${user.address.zipcode}</li>
+                                            <li class="list-group-item">Geo: <br>lat = ${user.address.geo.lat}<br>lng = ${user.address.geo.lat}</li>
+                                        </ul>
+                                    </div>
+                            </ul>
+                            </li>
+                            <li class="list-group-item">Company Details
+                                <a class="card-link" id="catch" data-toggle="collapse" href="#company${i}" role="button"
+                                    aria-expanded="false" aria-controls="company${i}">
+                                    - See more
+                                </a>
+            
+                                <div class="collapse" id="company${i}">
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item"><p class="phrase">${user.company.catchPhrase}</p></li>
+                                        <li class="list-group-item">Aria of interest: <br> ${user.company.bs}</li>
+                                    </ul>
+                                </div>
+                                </ul>
+                            </li>
+            
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+            ;
+        });
+
+        html += `</div>`;
+        container.html(html);
+        
+        
+    }
+    //loadModals(containerModals); <= bugged
+    loadUsers(container);
+
+    function initInteractions(el){
+
+        /* -function for intiation of the procedures */
+
+        function addClickEventToDeleteButtons(user, i, users) {
+            
+            /* 
+             - function takes as parameters user, i and users witch they are in order element of the array users, index of the array, array named users  for intiation of the procedures 
+             - the instruction that encapsulates has an .unbind("click") to deactivate the click event on line 238 
+             - after finding the link between the delete button and the container of the card the respective element of the array is removed and the list is refreshed
+             */
+                // console.log(targetedElement.closest("div[data-id]").getAttribute("uid") + `\n` + $(`div[data-index='${i}']`).getAttribute("uid"));
+                
+                $(`#delete${i}`).unbind("click").click(function(event){
+                    let targetedElement = event.currentTarget;
+                    console.log(targetedElement.closest(`div[data-userToBeDeleted='${i}']`, $(`#BusinessCard${i}`)));
+                    if (targetedElement.closest(`div[data-userToBeDeleted='${i}']`, $(`#BusinessCard${i}`))){
+                        console.log($(targetedElement));
+                            users.splice(i, 1);
+
+                        loadUsers(container);
+                }
+            });
+        }
+        
+        users.reduce( (accumulator, user, i, users) => { addClickEventToDeleteButtons(user, i, users); } );
+        
+    }
+    });
